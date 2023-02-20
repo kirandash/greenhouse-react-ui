f16c4201e3f6b283a96cf9e2b34453b5f6552fd6
kirandash <thekirankumardash@gmail.com>
2023-02-21 05:06:25 +0800

    ci: setup codecov after build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,43 @@
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import external from "rollup-plugin-peer-deps-external";
-import terser from "@rollup/plugin-terser";
-import postcss from "rollup-plugin-postcss";
-import babel from "rollup-plugin-babel";
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import external from 'rollup-plugin-peer-deps-external'
+import terser from '@rollup/plugin-terser'
+import postcss from 'rollup-plugin-postcss'
+import babel from 'rollup-plugin-babel'
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "dist/index.js",
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: "dist/index.esm.js",
-        format: "esm",
-        exports: "named",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      typescript({ tsconfig: "./tsconfig.json" }),
-      babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
-      }),
-      postcss({
-        plugins: [],
-        minimize: true,
-      }),
-      external(),
-      resolve(),
-      terser(),
-    ],
-  },
-];
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: 'dist/index.js',
+				format: 'cjs',
+				sourcemap: true,
+			},
+			{
+				file: 'dist/index.esm.js',
+				format: 'esm',
+				exports: 'named',
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			typescript({ tsconfig: './tsconfig.json' }),
+			babel({
+				exclude: 'node_modules/**',
+				presets: [
+					'@babel/preset-react',
+					'@babel/preset-env',
+					'@babel/preset-typescript',
+				],
+			}),
+			postcss({
+				plugins: [],
+				minimize: true,
+			}),
+			external(),
+			resolve(),
+			terser(),
+		],
+	},
+]
