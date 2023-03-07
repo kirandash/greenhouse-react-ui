9c9b0346305350c7f9a1102174aa06cffcae0a9f
kirandash <thekirankumardash@gmail.com>
2023-03-07 22:13:26 +0800

    fix: tailwind config for storybook building

--- style/tailwind.config.js
+++ style/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const greenhouse = require('greenhouse-react-ui/config')
+const greenhouse = require('../config')
 
 module.exports = greenhouse({
-	content: [],
+	content: ['./src/**/*.{ts,tsx,jsx,js}'],
 	theme: {
 		extend: {},
 	},
