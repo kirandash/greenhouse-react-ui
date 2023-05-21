a001254363f7d3ce826240dfde5cbbda2fcb15dc
Olakada Samuel <olakadasamuel@gmail.com>
2023-05-21 23:18:05 +0100

    feat(avatar): add new avatar component
    feat #5

---
+++ src/Avatar.tsx
@@ -0,0 +1,35 @@
+import React, { ImgHTMLAttributes, forwardRef, useContext } from 'react'
+import clsx from 'clsx'
+import { warn } from './utils/warning'
+import { ThemeContext } from './context/ThemeContext'
+
+// TODO: Tag validation - button, anchor
+type AvatarProps = ImgHTMLAttributes<HTMLImageElement> & {
+	size?: 'large' | 'regular' | 'small'
+	src: string
+	alt: string
+	className?: string
+}
+
+const Avatar = forwardRef<HTMLImageElement, AvatarProps>((props, ref) => {
+	const theme = useContext(ThemeContext)
+	const { avatar: avatarTheme } = theme
+	// Styles
+	const baseStyle = avatarTheme.base
+	const sizeStyles = {
+		large: avatarTheme.size.large,
+		regular: avatarTheme.size.regular,
+		small: avatarTheme.size.small,
+	}
+
+	const { size = 'regular', src, alt, className, ...other } = props
+
+	warn(!src, 'Avatar', 'you must pass a src to Avatar')
+	warn(!alt, 'Avatar', 'you must pass an alt to Avatar')
+
+	const cls = clsx(baseStyle, sizeStyles[size], className)
+
+	return <img ref={ref} className={cls} src={src} alt={alt} {...other} />
+})
+
+export default Avatar
