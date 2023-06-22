c04ec9461539aa066103c8991931f4779ada9582
Manuel Abascal <manuelsuarezabascal@gmail.com>
2023-06-21 22:26:31 -0400

    style(badge component): fix prettier format
    fix prettier format whcih prevent me from pushing the code
    
    #11

--- src/stories/Badge.stories.jsx
+++ src/stories/Badge.stories.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 import Badge from '../Badge'
 
 export default {
-    title: 'Components/Badge',
-    component: Badge,
-    argTypes: {},
+	title: 'Components/Badge',
+	component: Badge,
+	argTypes: {},
 }
 
 const Template = args => <Badge {...args}>Badge</Badge>
 
 export const Base = Template.bind({})
 Base.args = {
-    className: 'test-class-name',
-    color: 'base',
+	className: 'test-class-name',
+	color: 'base',
 }
 
 export const Neutral = Template.bind({})
 
 Neutral.args = {
-    className: 'test-class-name',
-    color: 'neutral',
+	className: 'test-class-name',
+	color: 'neutral',
 }
 
 export const Success = Template.bind({})
 Success.args = {
-    className: 'test-class-name',
-    color: 'success',
+	className: 'test-class-name',
+	color: 'success',
 }
 
 export const Danger = Template.bind({})
 Danger.args = {
-    className: 'test-class-name',
-    color: 'danger',
+	className: 'test-class-name',
+	color: 'danger',
 }
 
 export const Warning = Template.bind({})
 Warning.args = {
-    className: 'test-class-name',
-    color: 'warning',
+	className: 'test-class-name',
+	color: 'warning',
 }
--- src/tests/Badge.test.tsx
+++ src/tests/Badge.test.tsx
@@ -5,34 +5,34 @@ import Badge from '../Badge'
 import theme from '../themes/default'
 
 describe('Badge', () => {
-  it('renders without crashing', () => {
-    const { container } = render(<Badge>Test</Badge>)
-    expect(container.firstChild).toBeInTheDocument()
-  })
+	it('renders without crashing', () => {
+		const { container } = render(<Badge>Test</Badge>)
+		expect(container.firstChild).toBeInTheDocument()
+	})
 
-  it('renders children correctly', () => {
-    render(<Badge>Test</Badge>)
-    expect(screen.getByText('Test')).toBeInTheDocument()
-  })
+	it('renders children correctly', () => {
+		render(<Badge>Test</Badge>)
+		expect(screen.getByText('Test')).toBeInTheDocument()
+	})
 
-  it('applies the correct class for color', () => {
-    const { container } = render(<Badge color='danger'>Test</Badge>)
-    expect(container.firstChild).toHaveClass(theme.badge.danger)
-  })
+	it('applies the correct class for color', () => {
+		const { container } = render(<Badge color="danger">Test</Badge>)
+		expect(container.firstChild).toHaveClass(theme.badge.danger)
+	})
 
-  it('applies the correct class for neutral color when color prop is not provided', () => {
-    const { container } = render(<Badge>Test</Badge>)
-    expect(container.firstChild).toHaveClass(theme.badge.neutral)
-  })
+	it('applies the correct class for neutral color when color prop is not provided', () => {
+		const { container } = render(<Badge>Test</Badge>)
+		expect(container.firstChild).toHaveClass(theme.badge.neutral)
+	})
 
-  it('applies custom classes when provided', () => {
-    const { container } = render(<Badge className='custom-class'>Test</Badge>)
-    expect(container.firstChild).toHaveClass('custom-class')
-  })
+	it('applies custom classes when provided', () => {
+		const { container } = render(<Badge className="custom-class">Test</Badge>)
+		expect(container.firstChild).toHaveClass('custom-class')
+	})
 
-  it('forwards ref to the span element', () => {
-    const ref = React.createRef<HTMLSpanElement>()
-    render(<Badge ref={ref}>Test</Badge>)
-    expect(ref.current).toBeInstanceOf(HTMLSpanElement)
-  })
+	it('forwards ref to the span element', () => {
+		const ref = React.createRef<HTMLSpanElement>()
+		render(<Badge ref={ref}>Test</Badge>)
+		expect(ref.current).toBeInstanceOf(HTMLSpanElement)
+	})
 })
